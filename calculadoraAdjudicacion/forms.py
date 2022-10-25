88d860a2e39a0ec01e0af3482cfe8388fa592602
LeandroLamarca <lamarca.leandro@gmail.com>
2022-10-25 16:20:52 -0300

    Calculadora Horizonte: Primer commit

---
+++ calculadoraAdjudicacion/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+from django import forms
+from calculadoraAdjudicacion.models import PlanActual, PlanPorAdjudicar
+
+class PlanActualForm (ModelForm):
+    class Meta:
+        model = PlanActual
+        exclude = ['deuda']
+
+class PlanPorAdjudicarForm(ModelForm):
+    class Meta:
+        model = PlanPorAdjudicar
+        fields = '__all__'
+
+class AporteActualForm(forms.Form):
+    aporte_actual = forms.DecimalField(
+        label='Aporte mensual (antes de adjudicar)',
+        help_text='Monto mensual a aportar hasta el momento de la adjudicación',
+        min_value=1,
+        max_digits=8,
+        decimal_places=2
+        )
